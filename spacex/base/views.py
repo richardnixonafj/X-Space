9f79744527a09b53e877bb21c427b6668fb49be4
Richard Nixon <richardnixon@posteo.org>
2019-10-10 22:20:40 -0300

    Teste de renderizar na view

--- spacex/base/views.py
+++ spacex/base/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from spacex.base import lancamentos
 
+get_next_launch = lancamentos.get_next_launch()
 
 def home(request):
-    return HttpResponse('Olá Mundo')
+    return HttpResponse(get_next_launch)
