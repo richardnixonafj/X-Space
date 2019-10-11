e785b8f69defe54532efcea69e1955cedf4e227c
Richard Nixon <richardnixon@posteo.org>
2019-10-10 23:42:35 -0300

    Add timeout

--- spacex/base/api.py
+++ spacex/base/api.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def getx(endpoint, method="", query={}):
-    request_url = "https://api.spacexdata.com/v3/{end}/{meth}?pretty=true".format(end=endpoint, meth=method)
-    res = requests.get(request_url, params=query)
+def getx(endpoint, method=""):
+    request_url = "https://api.spacexdata.com/v3/{end}/{meth}?pretty=true".format(end=endpoint, meth=method, timeout=0.001)
+    res = requests.get(request_url)
 
     if not res.ok:
         res.raise_for_status()
