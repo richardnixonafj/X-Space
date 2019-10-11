80e7ead5ac5c2d24a97a7bca61cc0466eb957631
Richard Nixon <richardnixon@posteo.org>
2019-10-11 19:29:23 -0300

    Add Sentry

--- spacex/settings.py
+++ spacex/settings.py
@@ -9,13 +9,14 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import sentry_sdk
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+# Integration Sentry
+sentry_sdk.init("https://5eb38a48e173455ba26d9c4351a35f1b@sentry.io/1777295")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
