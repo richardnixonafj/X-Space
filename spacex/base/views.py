aa5e9bdc97dfe008eafd50010810864347799b8e
Richard Nixon <richardnixon@posteo.org>
2019-10-11 23:50:21 -0300

    Update views.py
    fix linter

--- spacex/base/views.py
+++ spacex/base/views.py
@@ -5,14 +5,18 @@ from spacex.base import lancamentos
 def home(request):
     return HttpResponse('Desafio Zapay')
 
+
 def proxlanc(request):
     return HttpResponse(lancamentos.get_proximo_lancamento())
+
 
 def ultlanc(request):
     return HttpResponse(lancamentos.get_ultimo_lancamento())
 
+
 def proxslancs(request):
     return HttpResponse(lancamentos.get_proximos_lancamentos())
+
 
 def lancspas(request):
     return HttpResponse(lancamentos.get_lancamentos_passados())
